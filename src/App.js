8f568a7197bb49b79f173fd1c0379cf24e7da197
Casey James <casey@khaosophy.com>
2021-03-08 11:56:25 -0500

    layed out the game board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Card from './components/Card';
+import './styles/playmat.css';
+
 function App() {
+  let cardArray = new Array(24);
+  for(let i = 0; i < cardArray.length; i++){
+    cardArray[i] = i + 1;
+  }
+  console.log(cardArray);
   return (
     <div className="App">
-      
+      <div className="playmat">
+        {cardArray.map(cardId => (
+          <Card key={cardId} symbol="Hello World" />
+        ))}
+      </div>
     </div>
   );
 }
